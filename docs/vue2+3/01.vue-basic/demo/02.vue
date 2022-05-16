5848dc9eb6e7314470ad10e91374d57ad5eb4484
henry <lh69jh@gmail.com>
2022-05-16 23:23:03 +0800

    模板语法，数据绑定

---
+++ docs/vue2+3/01.vue-basic/demo/02.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>插值语法</h1>
+    <h3>你好，{{ name }}</h3>
+    <hr />
+    <h1>指令语法</h1>
+    <a v-bind:href="school.url.toUpperCase()" x="hello">点我去{{ school.name }}学习 1</a>
+    <a :href="school.url" x="hello">点我去{{ school.name }}学习 2</a>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'jack',
+      school: {
+        name: '尚硅谷',
+        url: 'http://www.atguigu.com'
+      }
+    }
+  }
+}
+</script>
---
+++ docs/vue2+3/01.vue-basic/demo/03.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p>普通写法</p>
+    单向数据绑定：<input type="text" v-bind:value="name" /><br />
+    双向数据绑定：<input type="text" v-model:value="name" /><br />
+
+    <p>简写</p>
+    单向数据绑定：<input type="text" :value="name" /><br />
+    双向数据绑定：<input type="text" v-model="name" /><br />
+
+    <p>如下代码是错误的，因为 v-model 只能应用在表单类元素（输入类元素）上</p>
+    <h2 v-model:x="name">你好啊</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: '尚硅谷'
+    }
+  }
+}
+</script>
