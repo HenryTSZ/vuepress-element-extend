932acd1a91f1505d31c9ba34f84255948b7737bf
henry <lh69jh@gmail.com>
2022-05-18 23:16:54 +0800

    el,data, MVVM,数据代理

---
+++ docs/vue2+3/01.vue-basic/demo/04.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>你好，{{ name }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    console.log('@@@', this) //此处的 this 是 Vue 实例对象
+    return {
+      name: '尚硅谷'
+    }
+  }
+}
+</script>
---
+++ docs/vue2+3/01.vue-basic/demo/05.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>学校名称：{{ name }}</h1>
+    <h1>学校地址：{{ address }}</h1>
+    <!-- <h1>测试一下 1：{{ 1 + 1 }}</h1>
+    <h1>测试一下 2：{{ $options }}</h1>
+    <h1>测试一下 3：{{ $emit }}</h1>
+    <h1>测试一下 4：{{ _c }}</h1> -->
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return { name: '尚硅谷', address: '北京' }
+  }
+}
+</script>
