0813fd6f2b26f161e293b4ea69fc36898886f924
henry <lh69jh@gmail.com>
2022-06-02 22:41:04 +0800

    监测数据改变的原理

---
+++ docs/vue2+3/01.vue-basic/demo/14-1.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h2>人员列表</h2>
+    <p>点击一个按钮测试功能后，如需测试另一个按钮的功能，请先刷新页面</p>
+    <button @click="updateMei">更新马冬梅的信息(奏效)</button>
+    <button @click="updateMei2">更新马冬梅的信息(不奏效)</button>
+    <ul>
+      <li v-for="p of persons" :key="p.id">{{ p.name }}-{{ p.age }}-{{ p.sex }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      persons: [
+        { id: '001', name: '马冬梅', age: 30, sex: '女' },
+        { id: '002', name: '周冬雨', age: 31, sex: '女' },
+        { id: '003', name: '周杰伦', age: 18, sex: '男' },
+        { id: '004', name: '温兆伦', age: 19, sex: '男' }
+      ]
+    }
+  },
+  methods: {
+    updateMei() {
+      this.persons[0].name = '马老师' //奏效
+      this.persons[0].age = 50 //奏效
+      this.persons[0].sex = '男' //奏效
+    },
+    updateMei2() {
+      this.persons[0] = { id: '001', name: '马老师', age: 50, sex: '男' } //不奏效
+    }
+  }
+}
+</script>
---
+++ docs/vue2+3/01.vue-basic/demo/14-2.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    请在控制台查看输出
+  </div>
+</template>
+
+<script>
+export default {
+  created() {
+    let data = {
+      name: '尚硅谷',
+      address: '北京'
+    }
+
+    //创建一个监视的实例对象，用于监视 data 中属性的变化
+    const obs = new Observer(data)
+    console.log(obs)
+
+    //准备一个 vm 实例对象
+    let vm = {}
+    vm._data = data = obs
+
+    function Observer(obj) {
+      //汇总对象中所有的属性形成一个数组
+      const keys = Object.keys(obj)
+      //遍历
+      keys.forEach(k => {
+        Object.defineProperty(this, k, {
+          get() {
+            return obj[k]
+          },
+          set(val) {
+            console.log(`${k}被改了，我要去解析模板，生成虚拟 DOM.....我要开始忙了`)
+            obj[k] = val
+          }
+        })
+      })
+    }
+  }
+}
+</script>
