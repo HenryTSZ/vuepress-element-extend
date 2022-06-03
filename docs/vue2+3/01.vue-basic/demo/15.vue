ec71bef6136b21cd8397bcf685294606ba6f46f4
henry <lh69jh@gmail.com>
2022-06-03 22:40:22 +0800

    收集表单数据

---
+++ docs/vue2+3/01.vue-basic/demo/15.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <form @submit.prevent="demo">
+      账号：<input type="text" v-model.trim="userInfo.account" /> <br /><br />
+      密码：<input type="password" v-model="userInfo.password" /> <br /><br />
+      年龄：<input type="number" v-model.number="userInfo.age" /> <br /><br />
+      性别：男<input type="radio" name="sex" v-model="userInfo.sex" value="male" /> 女<input
+        type="radio"
+        name="sex"
+        v-model="userInfo.sex"
+        value="female"
+      />
+      <br /><br />
+      爱好：学习<input type="checkbox" v-model="userInfo.hobby" value="study" /> 打游戏<input
+        type="checkbox"
+        v-model="userInfo.hobby"
+        value="game"
+      />
+      吃饭<input type="checkbox" v-model="userInfo.hobby" value="eat" /> <br /><br />
+      所属校区
+      <select v-model="userInfo.city">
+        <option value="">请选择校区</option>
+        <option value="beijing">北京</option>
+        <option value="shanghai">上海</option>
+        <option value="shenzhen">深圳</option>
+        <option value="wuhan">武汉</option>
+      </select>
+      <br /><br />
+      其他信息：
+      <textarea v-model.lazy="userInfo.other"></textarea> <br /><br />
+      <input type="checkbox" v-model="userInfo.agree" />阅读并接受<a href="http://www.atguigu.com"
+        >《用户协议》</a
+      >
+      <button>提交</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userInfo: {
+        account: '',
+        password: '',
+        age: 18,
+        sex: 'female',
+        hobby: [],
+        city: 'beijing',
+        other: '',
+        agree: ''
+      }
+    }
+  },
+  methods: {
+    demo() {
+      console.log(JSON.stringify(this.userInfo))
+    }
+  }
+}
+</script>
