67b1a866e8efb4ab061a56ab35a74ce4740b4b1f
henry <lh69jh@gmail.com>
2022-06-07 23:27:35 +0800

    自定义指令

---
+++ docs/vue2+3/01.vue-basic/demo/18-1.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <h2>{{ name }}</h2>
+    <h2>当前的n值是：<span v-text="n"></span></h2>
+    <!-- <h2>放大10倍后的n值是：<span v-big-number="n"></span> </h2> -->
+    <h2>放大10倍后的n值是：<span v-big="n"></span></h2>
+    <button @click="n++">点我n+1</button>
+    <hr />
+    <input type="text" v-fbind:value="n" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: '尚硅谷',
+      n: 1
+    }
+  },
+  directives: {
+    //big函数何时会被调用？1.指令与元素成功绑定时（一上来）。2.指令所在的模板被重新解析时。
+    /* 'big-number'(element,binding){
+        // console.log('big')
+        element.innerText = binding.value * 10
+      }, */
+    big(element, binding) {
+      console.log('big', this) //注意此处的this是window
+      // console.log('big')
+      element.innerText = binding.value * 10
+    },
+    fbind: {
+      //指令与元素成功绑定时（一上来）
+      bind(element, binding) {
+        element.value = binding.value
+      },
+      //指令所在元素被插入页面时
+      inserted(element, binding) {
+        setTimeout(() => {
+
+          element.focus()
+        }, 1000);
+      },
+      //指令所在的模板被重新解析时
+      update(element, binding) {
+        element.value = binding.value
+      }
+    }
+  }
+}
+</script>
