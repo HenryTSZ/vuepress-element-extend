d7db359e9ebf5d78f665b1ea8792423339891c9e
henry <lh69jh@gmail.com>
2022-07-02 23:16:25 +0800

    ref

---
+++ docs/vue2+3/03.vue-cli/demo/02.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1 v-text="msg" ref="title"></h1>
+    <button ref="btn" @click="showDOM">点我输出上方的 DOM 元素</button>
+    <School ref="sch" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      msg: '欢迎学习 Vue！'
+    }
+  },
+  methods: {
+    showDOM() {
+      console.log(this.$refs.title) //真实 DOM 元素
+      console.log(this.$refs.btn) //真实 DOM 元素
+      console.log(this.$refs.sch) //School 组件的实例对象（vc）
+    }
+  }
+}
+</script>
