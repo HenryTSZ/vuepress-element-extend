b31b73264b3942b2bac969bf154eba65351c8add
henry <lh69jh@gmail.com>
2022-07-05 22:52:36 +0800

    plugis

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,7 +1,9 @@
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import plugins from './plugins'
 
 export default ({ Vue }) => {
   Vue.config.keyCodes.huiche = 13 // 定义了一个别名按键
   Vue.use(ElementUI)
+  Vue.use(plugins)
 }
---
+++ docs/vue2+3/03.vue-cli/demo/05.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <School3 />
+    <hr />
+    <Student3 />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
