28a4eba10443a702590f438fe1432f1c726f8184
henry <lh69jh@gmail.com>
2022-07-07 22:52:05 +0800

    scoped

---
+++ docs/vue2+3/03.vue-cli/demo/06.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1 class="demo-title">你好啊</h1>
+    <School4 />
+    <Student4 />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+.demo-title {
+  color: red;
+}
+</style>
