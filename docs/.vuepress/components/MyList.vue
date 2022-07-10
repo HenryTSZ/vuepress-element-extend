e54a26d3c10612869f847b4fca13c9556ce83743
henry <lh69jh@gmail.com>
2022-07-10 23:13:37 +0800

    todoList

---
+++ docs/.vuepress/components/MyList.vue
@@ -0,0 +1,38 @@
+<template>
+  <ul class="todo-main">
+    <MyItem
+      v-for="todoObj in todos"
+      :key="todoObj.id"
+      :todo="todoObj"
+      :checkTodo="checkTodo"
+      :deleteTodo="deleteTodo"
+    />
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'MyList',
+  //声明接收 App 传递过来的数据，其中 todos 是自己用的，checkTodo 和 deleteTodo 是给子组件 MyItem 用的
+  props: ['todos', 'checkTodo', 'deleteTodo']
+}
+</script>
+
+<style scoped>
+/*main*/
+.todo-main {
+  margin-left: 0px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+</style>
---
+++ docs/vue2+3/03.vue-cli/demo/07.vue
@@ -0,0 +1,92 @@
+<template>
+  <div id="root">
+    <div class="todo-container">
+      <div class="todo-wrap">
+        <MyHeader :addTodo="addTodo" />
+        <MyList :todos="todos" :checkTodo="checkTodo" :deleteTodo="deleteTodo" />
+        <MyFooter :todos="todos" :checkAllTodo="checkAllTodo" :clearAllTodo="clearAllTodo" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      //由于 todos 是 MyHeader 组件和 MyFooter 组件都在使用，所以放在 App 中（状态提升）
+      todos: [
+        { id: '001', title: '抽烟', done: true },
+        { id: '002', title: '喝酒', done: false },
+        { id: '003', title: '开车', done: true }
+      ]
+    }
+  },
+  methods: {
+    //添加一个 todo
+    addTodo(todoObj) {
+      this.todos.unshift(todoObj)
+    },
+    //勾选 or 取消勾选一个 todo
+    checkTodo(id) {
+      this.todos.forEach(todo => {
+        if (todo.id === id) todo.done = !todo.done
+      })
+    },
+    //删除一个 todo
+    deleteTodo(id) {
+      this.todos = this.todos.filter(todo => todo.id !== id)
+    },
+    //全选 or 取消全选
+    checkAllTodo(done) {
+      this.todos.forEach(todo => {
+        todo.done = done
+      })
+    },
+    //清除所有已经完成的 todo
+    clearAllTodo() {
+      this.todos = this.todos.filter(todo => {
+        return !todo.done
+      })
+    }
+  }
+}
+</script>
+
+<style>
+/*base*/
+.btn {
+  display: inline-block;
+  padding: 4px 12px;
+  margin-bottom: 0;
+  font-size: 14px;
+  line-height: 20px;
+  text-align: center;
+  vertical-align: middle;
+  cursor: pointer;
+  box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);
+  border-radius: 4px;
+}
+.btn-danger {
+  color: #fff;
+  background-color: #da4f49;
+  border: 1px solid #bd362f;
+}
+.btn-danger:hover {
+  color: #fff;
+  background-color: #bd362f;
+}
+.btn:focus {
+  outline: none;
+}
+.todo-container {
+  width: 600px;
+  margin: 0 auto;
+}
+.todo-container .todo-wrap {
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+}
+</style>
