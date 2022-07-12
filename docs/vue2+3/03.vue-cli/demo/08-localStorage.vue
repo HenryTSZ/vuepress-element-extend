9b0eb9032c2f234ce30b04b3a953d68d60b82616
henry <lh69jh@gmail.com>
2022-07-12 23:19:07 +0800

    webStorage

---
+++ docs/vue2+3/03.vue-cli/demo/08-localStorage.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h2>localStorage</h2>
+    <button @click="saveData()">点我保存一个数据</button>
+    <button @click="readData()">点我读取一个数据</button>
+    <button @click="deleteData()">点我删除一个数据</button>
+    <button @click="deleteAllData()">点我清空一个数据</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  methods: {
+    saveData() {
+      localStorage.setItem('msg', 'hello!!!')
+      localStorage.setItem('msg2', 666)
+      let p = { name: '张三', age: 18 }
+      localStorage.setItem('person', JSON.stringify(p))
+    },
+    readData() {
+      console.log('msg:', localStorage.getItem('msg'))
+      console.log('msg2:', localStorage.getItem('msg2'))
+
+      const result = localStorage.getItem('person')
+      console.log('person:', JSON.parse(result))
+
+      console.log('msg3:', localStorage.getItem('msg3'))
+    },
+    deleteData() {
+      localStorage.removeItem('msg2')
+    },
+    deleteAllData() {
+      localStorage.clear()
+    }
+  }
+}
+</script>
---
+++ docs/vue2+3/03.vue-cli/demo/08-sessionStorage.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h2>sessionStorage</h2>
+    <button @click="saveData()">点我保存一个数据</button>
+    <button @click="readData()">点我读取一个数据</button>
+    <button @click="deleteData()">点我删除一个数据</button>
+    <button @click="deleteAllData()">点我清空一个数据</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    saveData() {
+      sessionStorage.setItem('msg', 'hello!!!')
+      sessionStorage.setItem('msg2', 666)
+      let p = { name: '张三', age: 18 }
+      sessionStorage.setItem('person', JSON.stringify(p))
+    },
+    readData() {
+      console.log('msg:', sessionStorage.getItem('msg'))
+      console.log('msg2:', sessionStorage.getItem('msg2'))
+
+      const result = sessionStorage.getItem('person')
+      console.log('person:', JSON.parse(result))
+
+      console.log('msg3:', sessionStorage.getItem('msg3'))
+    },
+    deleteData() {
+      sessionStorage.removeItem('msg2')
+    },
+    deleteAllData() {
+      sessionStorage.clear()
+    }
+  }
+}
+</script>
