01a335c24ee92e47689be5089580a3ed6e25d89e
henry <lh69jh@gmail.com>
2022-08-10 22:52:33 +0800

    todoList

---
+++ docs/.vuepress/components/MyList4.vue
@@ -0,0 +1,50 @@
+<template>
+  <ul class="todo-main">
+    <transition-group name="todo" appear>
+      <MyItem3 v-for="todoObj in todos" :key="todoObj.id" :todo="todoObj" />
+    </transition-group>
+  </ul>
+</template>
+
+<script>
+export default {
+  //声明接收 App 传递过来的数据
+  props: ['todos']
+}
+</script>
+
+<style scoped>
+/*main*/
+.todo-main {
+  margin-left: 0px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+
+.todo-enter-active {
+  animation: atguigu 0.5s linear;
+}
+
+.todo-leave-active {
+  animation: atguigu 0.5s linear reverse;
+}
+
+@keyframes atguigu {
+  from {
+    transform: translateX(100%);
+  }
+  to {
+    transform: translateX(0px);
+  }
+}
+</style>
